ba0accb307ded6bab8693f38cfbc7fd4a5391d58
manelsen <emanuelmoura@gmail.com>
2026-04-15 22:47:35 -0300

    fix(executor): raise approval timeout from 5min to 10min

--- lib/pincer/core/executor.ex
+++ lib/pincer/core/executor.ex
@@ -21,7 +21,7 @@ defmodule Pincer.Core.Executor do
   alias Pincer.Utils.Text
 
   @max_recursion_depth 100
-  @approval_timeout_ms Application.compile_env(:pincer, :approval_timeout_ms, 300_000)
+  @approval_timeout_ms Application.compile_env(:pincer, :approval_timeout_ms, 600_000)
   @tool_result_max_chars Application.compile_env(:pincer, :tool_result_max_chars, 32_000)
 
   # Maximum size for inline data (6MB default for Gemini/Google)
